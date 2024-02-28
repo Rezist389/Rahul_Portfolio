ee15d1b72efec985aed44c63acac5ed7b55e39c4
Rezist389 <2020bit033@sggs.ac.in>
2024-02-28 14:36:25 +0530

    r1

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Rezist389",
   resume:
-    "https://drive.google.com/file/d/1Lx1VHDyPM5YigSB8_xoSzgKQ5Cc2A4hF/view?usp=sharing",
+    "https://drive.google.com/file/d/1G1AWGl_DrE_21IivVmseg2Vu81ckfES3/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rahul-wankhede-030848204",
   twitter: "https://twitter.com/Rezist38",
   insta: "https://www.instagram.com/_rahul_rw/",
@@ -167,7 +167,7 @@ export const experiences = [
     role: "Full Stack Inter Intern",
     company: "Numetry Technologies",
     date: "Jan 2024 - Present",
-    desc: "Working on developing a trading/securities platform, Collaborating with the team to perform various Frontend Tasks, Engineering Tasks,POC Tasks",
+    desc: "Working on developing Web apps, Collaborating with the team to perform various Frontend Tasks, Engineering Tasks,POC Tasks",
     skills: [
       "ReactJS",
       "NodeJs",
